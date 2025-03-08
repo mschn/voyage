2c378589e7727af2d258f3fbf15b64eed89e2d69
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2025-03-08 21:10:33 +0100

    feat: move ngx-voyage to separate project

--- projects/ngx-voyage/src/lib/model/message.ts
+++
@@ -1,8 +0,0 @@
-export interface Message {
-  text: string;
-  type: 'info' | 'warn' | 'error';
-}
-
-export function isMessage(m: any): m is Message {
-  return m?.text && m?.type;
-}
--- projects/ngx-voyage/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-voyage
- */
-
-export * from './lib/model/model';
-export * from './lib/model/message';
-export * from './lib/ngx-voyage/ngx-voyage.component';
